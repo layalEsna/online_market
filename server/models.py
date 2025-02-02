6945f738da62f3fe30d9e748dbe2de38539debce
Amene <layla_1380@yahoo.com>
2025-02-01 23:46:19 -0600

    Add User model with attributes: id, username, and password

--- server/models.py
+++ server/models.py
@@ -4,3 +4,31 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    _hash_password = db.Column(db.String(128), nullable=False)
+
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    image = db.Column(db.String)
+    price = db.Column(db.Numeric(10,2), nullable=False)
+
+
+class Purchase(db.Model, SerializerMixin):
+    __tablename__ = 'purchases'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
+    delivery_address = db.Column(db.String(255), nullable=False)
+    payment_method = db.Column(db.String(50), nullable=False)
