e0dbfdae5e6fb5a15819d44d1ec42dc169bd9972
Amene <layla_1380@yahoo.com>
2025-02-05 22:01:15 -0600

    Add PurchaseConfirmation component to display confirmation message after checkout

---
+++ client/src/components/PurchaseConfirmation.js
@@ -0,0 +1,14 @@
+
+
+function PurchaseConfirmation() {
+
+    return (
+        <div>
+            <h1>Purchase Confirmed!</h1>
+            <h3>Looking forward to seeing you again!</h3>
+        </div>
+    )
+
+}
+
+export default PurchaseConfirmation
