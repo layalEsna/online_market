5be92aeeb16c1087dec10dc0f7b2a7fc050ce278
Amene <layla_1380@yahoo.com>
2025-02-09 13:46:45 -0600

    Add handleLogout function to NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,16 +1,41 @@
 
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+
 
 function NavBar() {
     
+    const navigate = useNavigate()
+    function handleLogout() {
+        fetch('http://127.0.0.1:5555/logout', {
+            method: 'DELETE',
+        })
+            .then(res => {
+                if (res.ok) {
+                navigate('/login')
+                } else {
+                    console.error('Logout failed.')
+            }
+            })
+            .catch(e => {
+            console.error(`Error during logout: ${e}`)
+        })
+    }
+    
+
     return (
-        <nav>
-            {/* <Link to='/signup'></Link>
-            <Link to='/login'></Link> */}
+      
+            <nav>
+                
+            
             <Link to='/sellers'>Market View</Link>
             
             <Link to='/cart'>Cart</Link>
-       </nav>
+            <button className='btn' onClick={handleLogout}>logout</button>
+       
+
+            </nav>
+           
+            
 )
 
 }
